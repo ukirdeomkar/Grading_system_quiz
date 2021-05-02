fb5fd7a60df4fdca9d2a854672f2353d768449ea
ukirdeomkar <ukirdeomkar4@gmail.com>
2021-05-02 11:21:18 +0530

    unnecessary files removed

--- fetch_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Include database connection
-if($_POST['rowid']) {
-    $id = $_POST['rowid']; //escape string
-    // Run the Query
-    // Fetch Records
-    // Echo the data you want to show in modal
- }
-?>
